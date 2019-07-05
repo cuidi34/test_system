5c37c136d29dcd26ae857f7c5e4b47a8ea0b2f38
cuidi34@gmail.com <cuidi34@gmail.com>
2019-07-05 16:43:38 +0800

    Annotate ShortEssayQuestion.java

--- src/question/ShortEssayQuestion.java
+++ src/question/ShortEssayQuestion.java
@@ -1,36 +1,36 @@
 package question;
 
-import anwser.Answer;
-import anwser.TextAnswer;
+import anwser.Answer; /** import relation -> Answer.java */
+import anwser.TextAnswer; /** import relation -> TextAnswer.java */
 
-public class ShortEssayQuestion extends PromptQuestion {
+public class ShortEssayQuestion extends PromptQuestion { /** extend relation -> PromptQuestion.java */
 	
-	TextAnswer answer;
+	TextAnswer answer; /** contain relation -> TextAnswer.java */
 	
 	public ShortEssayQuestion() {
-		super(2);
+		super(2); /** call relation -> PromptQuestion.java */
 		// TODO Auto-generated constructor stub
 	}	
 	
 	public String getQuestion(){
-		return "Text: "+prompt;
+		return "Text: "+prompt; /** use relation -> Question.java */
 	}
 	
 	public void setAnswer(String answer) {
 		// TODO Auto-generated method stub
 		
-		this.answer = new TextAnswer();
-		this.answer.setAnswer(answer);
+		this.answer = new TextAnswer(); /** Create relation -> TextAnswer.java */
+		this.answer.setAnswer(answer); /** Call relation -> TextAnswer.java */
 	}
 
-	public Answer getAnswer() {
+	public Answer getAnswer() { /** return relation -> Answer.java */
 		// TODO Auto-generated method stub
-		return answer;
+		return answer; /** Use relation -> TextAnswer.java */
 	}
 
 
 	@Override
-	public boolean match(Answer answer) {
-		return this.answer.match(answer);
+	public boolean match(Answer answer) { /** parameter relation -> Answer.java */
+		return this.answer.match(answer); /** Call relation -> TextAnswer.java */
 	}
 }
